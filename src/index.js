0b984b24c8b3364c24573020f20a126b89393476
Adarshcodes <adarsh.lrnr@gmail.com>
2022-05-18 23:50:12 +0530

    creating a new directory component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./categories.styles.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
