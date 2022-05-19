5e4e6ca467c7fbc3c7d28ca24a134e256eb2e4fa
Adarshcodes <adarsh.lrnr@gmail.com>
2022-05-20 00:14:29 +0530

    adding routes outlet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { Routes, Route, Outlet } from "react-router-dom";
 import Home from "./routes/home/home.component";
 
+const Navigation = () => {
+	return (
+		<div>
+			<div>
+				<h1>This is the navigation bar</h1>
+			</div>
+			<Outlet />
+		</div>
+	);
+};
+
+const Shop = () => {
+	return <div>I'm the shop page</div>;
+};
+
 const App = () => {
-	return <Home />;
+	return (
+		<Routes>
+			<Route path="/" element={<Navigation />}>
+				<Route index element={<Home />} />
+				<Route path="shop" element={<Shop />} />
+			</Route>
+		</Routes>
+	);
 };
 
 export default App;
